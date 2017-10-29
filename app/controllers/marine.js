cdc0bd7cd0d2a019f850ec6d2f53cbde8a92dd6f
Pierre de Leastar <pdeleastar@gmail.com>
2017-10-29 09:33:17 +0100

    request ship course information from marinetraffic module

--- app/controllers/marine.js
+++ app/controllers/marine.js
@@ -1,24 +1,53 @@
 'use strict';
 
 const marineUtils = require('./marineutils');
+var marinetraffic = require('marinetraffic');
+
+let count = 0;
+let allShips = [];
 
 exports.report = {
 
   handler: function (request, reply) {
     reply.view('report', {
       title: 'Ship Locations',
+      ships: allShips,
     });
   },
 
 };
 
+function readMsi(msi,  request, reply) {
+  marinetraffic(msi, function (err, result) {
+        count++;
+        if (err) {
+          marineUtils.reportError(err, msi);
+        } else {
+          var shipCourse = marineUtils.getMsiDetails(result);
+          shipCourse.msi = msi;
+          allShips.push(shipCourse);
+          if (count == 6) {
+            reply.view('report', {
+              title: 'Course List',
+              ships: allShips,
+            });
+          }
+        }
+      }
+  );
+}
+
 exports.refresh = {
 
   handler: function (request, reply) {
+    count = 0;
     let msi = marineUtils.getMsiNumbersFromForm(request.payload);
-    reply.view('report', {
-      title: 'Ship Locations',
-    });
+    readMsi(msi[0], request, reply);
+    readMsi(msi[1], request, reply);
+    readMsi(msi[2], request, reply);
+    readMsi(msi[3], request, reply);
+    readMsi(msi[4], request, reply);
+    readMsi(msi[5], request, reply);
   },
 
 };
